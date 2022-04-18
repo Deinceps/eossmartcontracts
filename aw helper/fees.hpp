ec409d373292e7bcffd2770ed82029e1683856ba
my su[erewds <san.costin2012@yandex.ru>
2022-04-18 15:09:53 +0300

    Update and rename fee.hpp to fees.hpp

---
+++ aw helper/fees.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <eosio/asset.hpp>
+#include <eosio/eosio.hpp>
+
+#include <string>
+
+namespace eosiosystem {
+   class system_contract;
+}
+
+namespace eosio {
+
+   using std::string;
+
+   class [[eosio::contract("fees")]] fees : public contract {
+      public:
+	using contract::contract;
+	[[eosio::action]]
+	void fee(const name& a, const asset& v);
+         
+	using fee_action = eosio::action_wrapper<"fee"_n, &fees::fee>;
+	struct accounts
+	{
+		eosio::asset balance;
+		uint64_t primary_key() const {return balance.symbol.code().raw();}
+	};
+     typedef eosio::multi_index< eosio::name("accounts"), accounts > accounts_table;
+
+   };
+}
