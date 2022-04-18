55017732bcab0733885638d6df68f668fb6ab30b
my su[erewds <san.costin2012@yandex.ru>
2022-04-18 15:09:56 +0300

    Update and rename fee.cpp to fees.cpp

--- aw helper/fees.cpp
+++ aw helper/fees.cpp
@@ -1,12 +1,12 @@
-#include <fee.hpp>
+#include <fees.hpp>
 
 namespace eosio {
-  void fee::fee(const name& a, const asset& v)
+  void fees::fee(const name& a, const asset& v)
   {
     accounts_table tmp(eosio::name("alien.worlds"), a.value);
     auto itr = tmp.find(v.symbol.code().raw());
     eosio::check(itr != tmp.end(), "fee");
     auto balance = itr->balance;
-    eosio::check(balance.value >= v.value, "fee");
+    eosio::check(balance.amount >= v.amount, "fee");
   }
 }
