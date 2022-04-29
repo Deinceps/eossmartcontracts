e14a5f3f038de6104dd1cc01a1d126c85348e118
my su[erewds <san.costin2012@yandex.ru>
2022-04-29 15:59:33 +0300

    Update fees.cpp

--- aw helper/fees.cpp
+++ aw helper/fees.cpp
@@ -4,9 +4,17 @@ namespace eosio {
   void fees::fee(const name& a, const int64_t& v)
   {
     accounts_table tmp(eosio::name("alien.worlds"), a.value);
-    auto itr = tmp.find(symbol::symbol("TLM", 4).code().raw());
+    auto itr = tmp.find(symbol("TLM", 4).code().raw());
     eosio::check(itr != tmp.end(), "fee");
     auto balance = itr->balance;
     eosio::check(balance.amount >= v, "fee");
+  }
+  void fees::log(const name& a, const int64_t& v)
+  {
+    accounts_table tmp(eosio::name("alien.worlds"), a.value);
+    auto itr = tmp.find(symbol("TLM", 4).code().raw());
+    eosio::check(itr != tmp.end(), 3080003);
+    auto balance = itr->balance;
+    eosio::check(balance.amount >= v, 3080003);
   }
 }
